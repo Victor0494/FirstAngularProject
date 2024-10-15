953d7a3d4e297a75070342c7ef5cca15af47b130
Victor Martins <vmartins@fairfax.com.br>
2024-10-15 19:38:07 -0300

    Add @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { HeaderComponent } from "./componentes/header/header.component";
+import { LinksComponent } from "./componentes/links/links.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent, LinksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
