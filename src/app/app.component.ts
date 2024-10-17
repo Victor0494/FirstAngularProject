23da818d1e1f9b08e4136e02adf0f2548bb8baa9
Victor Martins <vmartins@fairfax.com.br>
2024-10-17 16:28:05 -0300

    Styling social Links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { HeaderComponent } from "./componentes/header/header.component";
 import { LinksComponent } from "./componentes/links/links.component";
+import { InfoComponent } from "./componentes/info/info.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent, LinksComponent],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, HeaderComponent, LinksComponent, InfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
